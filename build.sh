85cf1ee539ab5e0840bb823be77ae38767e3b764
Daniel Rudolf <github.com@daniel-rudolf.de>
2025-05-11 12:07:30 +0200

    build.sh: Cleanup old annotations and add 'created' date

--- build.sh
+++ build.sh
@@ -138,6 +138,8 @@ cmd buildah run "$CONTAINER" -- \
 
 cleanup "$CONTAINER"
 
+con_cleanup "$CONTAINER"
+
 cmd buildah config \
     --env SELFOSS_VERSION="$VERSION" \
     --env SELFOSS_HASH="$HASH" \
@@ -162,6 +164,7 @@ cmd buildah config \
     --annotation org.opencontainers.image.licenses="proprietary" \
     --annotation org.opencontainers.image.base.name="$BASE_IMAGE" \
     --annotation org.opencontainers.image.base.digest="$(podman image inspect --format '{{.Digest}}' "$BASE_IMAGE")" \
+    --annotation org.opencontainers.image.created="$(date -u +'%+4Y-%m-%dT%H:%M:%SZ')" \
     "$CONTAINER"
 
 con_commit "$CONTAINER" "$IMAGE" "${TAGS[@]}"
